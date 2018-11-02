c2fb90a3111cb441e297da000960e4b59ef53f48
Marcel Laptop <mlhardy.mh@gmail.com>
2018-11-02 16:51:33 -0500

    For testing purposes I hard-coded in some addresses. The preliminary tests worked.

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ import json
 from api_key import my_key
 
 endpoint = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&"
-origins = input("Where from?:").replace(" ","+")
-destinations = input("Where to?:").replace(" ","+")
+#origins = input("Where from?:").replace(" ","+")
+#destinations = input("Where to?:").replace(" ","+")
+origins = "1902+Rosalie+Ridge+Dr+Huntsville+AL"
+destinations = "143+Tuscany+Lane+Vine+Grove+KY|704+Desoto+Rd+Huntsville+AL"
 nav_req = "origins={}&destinations={}&key={}".format(origins, destinations, my_key)
 
 request = endpoint + nav_req
 response = urllib.request.urlopen(request).read()
-distances = json.loads(response)
+data = json.loads(response)
 
+distances = data['rows'][0]['elements']
+for i in range(len(distances)):
+    
